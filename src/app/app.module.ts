a3446d4bc58b5fc228aeab2151dc445d5a758e32
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-11-19 22:29:41 +0000

    add new component hotel-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HotelListComponent
   ],
   imports: [
     BrowserModule
