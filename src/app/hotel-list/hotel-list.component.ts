eba08c4a30b5858bf68f21bbbbc2be8d85a474c2
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-11-20 22:24:25 +0000

    update price

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -12,7 +12,7 @@ export class HotelListComponent {
       hotelId: 1,
       hotelName: "BelCote",
       hotelDescription: "Petit endroit chic pour les couples",
-      hotelPrice: 20000,
+      hotelPrice: 2000,
       hotelLocation: "Abidjan",
       hotelImgUrl: "../../assets/img/hotel1.jpg",
       place: 20,
@@ -21,7 +21,7 @@ export class HotelListComponent {
       hotelId: 2,
       hotelName: "Président",
       hotelDescription: "L'excelence en terme d'hotal",
-      hotelPrice: 25000,
+      hotelPrice: 2500,
       hotelLocation: "Yamoussoukro",
       hotelImgUrl: "../../assets/img/hotel2.jpg",
       place: 30,
@@ -30,7 +30,7 @@ export class HotelListComponent {
       hotelId: 3,
       hotelName: "Assinie",
       hotelDescription: "Bronzé en toute quiétude",
-      hotelPrice: 15000,
+      hotelPrice: 1500,
       hotelLocation: "Assinie",
       hotelImgUrl: "../../assets/img/hotel3.jpg",
       place: 4,
@@ -39,7 +39,7 @@ export class HotelListComponent {
       hotelId: 4,
       hotelName: "Hotel Djef",
       hotelDescription: "Rencontrer votre âme soeur",
-      hotelPrice: 10000,
+      hotelPrice: 1000,
       hotelLocation: "Abidjan",
       hotelImgUrl: "../../assets/img/hotel4.jpg",
       place: 20,
@@ -48,7 +48,7 @@ export class HotelListComponent {
       hotelId: 5,
       hotelName: "Palace of King",
       hotelDescription: "for king and queen",
-      hotelPrice: 40000,
+      hotelPrice: 4000,
       hotelLocation: "Man",
       hotelImgUrl: "../../assets/img/hotel5.jpg",
       place: 100,
@@ -57,11 +57,19 @@ export class HotelListComponent {
       hotelId: 6,
       hotelName: "Mille Bougie",
       hotelDescription: "Uniquement réserver au jeu",
-      hotelPrice: 17000,
+      hotelPrice: 1700,
       hotelLocation: "Abidjan",
       hotelImgUrl: "../../assets/img/hotel6.jpg",
       place: 17,
     },
 
-  ]
+  ];
+
+  public hotelFilter= "mot";
+  
+  public showBadge!: boolean;
+  
+  public toggleIsNewBadge(): void{
+    this.showBadge = !this.showBadge;
+  }
 }
