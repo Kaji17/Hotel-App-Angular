02f000459bb7827dfefbf70ed3c680c93053dd32
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-11-22 14:38:03 +0000

    creat star-rating component and communication

---
+++ src/app/shared/component/star-rating/star-rating.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  templateUrl: './star-rating.component.html',
+  styleUrls: ['./star-rating.component.scss']
+})
+export class StarRatingComponent implements OnChanges {
+  public starWidth!: number;
+
+  @Input()
+  public rating = 2;
+
+  ngOnChanges() {
+    this.starWidth = this.rating * 125 / 5;
+  }
+
+
+}
