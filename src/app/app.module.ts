1b5cad5d1374308cf7b445de87ca525a3e0ed6e3
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-12-01 16:51:35 +0000

    add component, pipe module, httpmodule, ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HotelListComponent } from './hotel-list/hotel-list.component';
+import { HotelListComponent } from './hotels/hotel-list/hotel-list.component';
 import { registerLocaleData } from '@angular/common';
-import {NgPipesModule} from 'ngx-pipes';
+import { NgPipesModule } from 'ngx-pipes';
+import { HttpClientModule } from "@angular/common/http";
 
 import localFr from '@angular/common/locales/fr';
 import { StarRatingComponent } from './shared/component/star-rating/star-rating.component';
+import { HomeComponent } from './home/home.component';
+import { HotelDetailComponent } from './hotels/hotel-detail/hotel-detail.component';
+import { RouterModule } from '@angular/router';
+import { HotelDetailGuard } from './hotels/shared/guard/hotel-detail.guard';
 
 registerLocaleData(localFr, 'fr')
 
@@ -19,11 +24,24 @@ registerLocaleData(localFr, 'fr')
     HeaderComponent,
     HotelListComponent,
     StarRatingComponent,
+    HomeComponent,
+    HotelDetailComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     NgPipesModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: "home", component: HomeComponent},
+      {path: "hotels", component: HotelListComponent},
+      {
+        path: "hotels/:id", component: HotelDetailComponent,
+        canActivate: [HotelDetailGuard]
+      },
+      {path: "", redirectTo: "home", pathMatch: "full"},
+      {path: "**", redirectTo: "home", pathMatch: "full"}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
